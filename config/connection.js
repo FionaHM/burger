469f6071b0e028726721e2655b44f52f49938720
FH <FH@Fionas-MacBook-Air.local>
2017-01-26 11:08:49 -0600

    modified the schema.sql file

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL){
 	connection =mysql.createConnection({
 	  host     : 'localhost',
 	  user     : 'root',
-	  password : '01B00tKamp09',
+	  password : '',
 	  database : 'burgers_db'
 	});
 }
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 CREATE database burgers_db;
 
-drop table burgers;
 use burgers_db;
 
 CREATE table burgers (
@@ -8,7 +7,7 @@ id int auto_increment not null,
 burger_name VARCHAR(32) not null,
 devoured BOOLEAN DEFAULT FALSE,
 date_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP not null,
-createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+-- createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 PRIMARY KEY(id));
 
 select * from burgers;
